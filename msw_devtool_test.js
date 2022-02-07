bb397e363f1620d5818173c9ac2b8a7a4ae2dcb2
dnjstjr93 <dnjstjr93@gmail.com>
2022-02-07 15:27:18 +0900

    -test

--- msw_devtool_test.js
+++ msw_devtool_test.js
@@ -217,30 +217,27 @@ function local_msw_mqtt_connect(broker_ip, port) {
 
         local_msw_mqtt_client.on('connect', function () {
             console.log('[local_msw_mqtt_connect] connected to ' + broker_ip);
+            for (let idx in msw_sub_lib_topic) {
+                if (msw_sub_lib_topic.hasOwnProperty(idx)) {
+                    local_msw_mqtt_client.subscribe(msw_sub_lib_topic[idx]);
+                    console.log('[local_msw_mqtt] msw_sub_lib_topic[' + idx + ']: ' + msw_sub_lib_topic[idx]);
+                }
+            }
+            for (let idx in msw_sub_muv_topic) {
+                if (msw_sub_muv_topic.hasOwnProperty(idx)) {
+                    local_msw_mqtt_client.subscribe(msw_sub_muv_topic[idx]);
+                    console.log('[local_msw_mqtt] msw_sub_muv_topic[' + idx + ']: ' + msw_sub_muv_topic[idx]);
+                }
+            }
             for (let idx in msw_sub_fc_topic) {
                 if (msw_sub_fc_topic.hasOwnProperty(idx)) {
                     local_msw_mqtt_client.subscribe(msw_sub_fc_topic[idx]);
                     console.log('[local_msw_mqtt] msw_sub_fc_topic[' + idx + ']: ' + msw_sub_fc_topic[idx]);
                 }
             }
-            for (let idx in msw_sub_lib_topic) {
-                if (msw_sub_lib_topic.hasOwnProperty(idx)) {
-                    msw_mqtt_client.subscribe(msw_sub_lib_topic[idx]);
-                    console.log('[local_msw_mqtt] msw_sub_lib_topic[' + idx + ']: ' + msw_sub_lib_topic[idx]);
-                }
-            }
-            for (let idx in msw_sub_muv_topic) {
-                if (msw_sub_muv_topic.hasOwnProperty(idx)) {
-                    msw_mqtt_client.subscribe(msw_sub_muv_topic[idx]);
-                    console.log('[local_msw_mqtt] msw_sub_muv_topic[' + idx + ']: ' + msw_sub_muv_topic[idx]);
-                }
-            }
         });
 
         local_msw_mqtt_client.on('message', function (topic, message) {
-            if (message.toString() === 'OFF') {
-                console.log(message.toString());
-            }
             for (let idx in msw_sub_fc_topic) {
                 if (msw_sub_fc_topic.hasOwnProperty(idx)) {
                     if (topic === msw_sub_fc_topic[idx]) {
